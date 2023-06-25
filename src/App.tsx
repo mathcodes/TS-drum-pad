7b576a4846fbce2b3519d05a12935f5b21e51960
Jon Christie <jonpchristie@gmail.com>
2023-06-25 07:15:28 -0400

    setup

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+
+type DrumPadItem = {
+  id: string,
+  letter: string,
+  keyCode: number,
+  src: string
+};
+
+const App: React.FC = () => {
+
+
+
+  return (
+      <div
+        id="drum-machine"
+      >
+      </div>
+  );
+}
+
+export default App;
